8cf6a16d710ec9c04bf23a1e693078555affbe84
Austin Fuller <auxchar@gmail.com>
2015-12-18 16:18:41 -0600

    Added anvil restriction for ttk senpai.

--- src/main/java/auxchar/mods/repairshower/TooltipListener.java
+++ src/main/java/auxchar/mods/repairshower/TooltipListener.java
@@ -3,13 +3,18 @@ package auxchar.mods.repairshower;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.inventory.ContainerRepair;
+import net.minecraft.inventory.Container;
 
 public class TooltipListener {
     @SubscribeEvent
     public void onTooltip(ItemTooltipEvent e) {
         int cost = e.itemStack.getRepairCost();
         if (cost > 0) {
-            e.toolTip.add(EnumChatFormatting.RED.toString()+"Repair Cost: "+cost);
+            Container anvil = e.entityPlayer.openContainer;
+            if (anvil != null && anvil instanceof ContainerRepair) {
+                e.toolTip.add(EnumChatFormatting.RED.toString()+"Repair Cost: "+cost);
+            }
         }
     }
 }
